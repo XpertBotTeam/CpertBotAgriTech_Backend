9051e764bedeaa3256f697f1509dddbfd8b18d49
ahmadassaf12 <ahmedaassaf@gmail.com>
2023-08-21 22:22:03 +0300

    implement api tegistration and login

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email','password']);
+
+        if(Auth::attempt($credentials))
+        {
+            // $user = Auth::user()->id;
+            $user = $request->user();
+            $access_token = $user->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'success'=>true,
+                'token'=>$access_token,
+                'message'=>'User logged-in successfuly'
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'success'=>false,
+                'message'=>'wrong username or password'
+            ]);
+        }
+    }
+    public function register(Request $request)
+    {
+
+        $user = User::where('email',$request->email)->first();
+
+        if(is_null($user))
+        {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'status'=>true,
+                'message' => 'User Creeated Successfully',
+                'token' => $token
+            ]);
+
+        }
+        else
+        {
+            return response()->json([
+                'status'=>false,
+                'message'=>'User already exists'
+            ]);
+        }
+
+
+    }
+}
--- database/migrations/2023_08_20_144033_create_reports_table.php
+++ database/migrations/2023_08_20_144033_create_reports_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('disease_id')->constrained()->onDelete('cascade');
+            $table->text('body');
             $table->timestamps();
         });
     }
