6b3807d30f90ef6b9ac6f93b84079d52ea754b7f
ahmadassaf12 <ahmedaassaf@gmail.com>
2023-08-25 17:08:37 +0300

    change get to post

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -14,6 +14,7 @@ class AuthController extends Controller
     {
         $credentials = $request->only(['email','password']);
 
+
         if(Auth::attempt($credentials))
         {
             // $user = Auth::user()->id;
